be83f7de85b90fb6cf2a6627a7907596f483dadf
oliveruhlar <oliver.uhlar@pantheon.tech>
2020-11-27 14:04:11 +0100

    final final

--- ping_vms.py
+++ ping_vms.py
@@ -3,12 +3,27 @@ import time
 
 
 def print_ifconffig(vm):
-    vm_stdin, vm_stdout, vm_stderr = vm.exec_command("ip -o address")
+    vm_stdin, vm_stdout, vm_stderr = vm.exec_command("ifconfig")
     lines = vm_stdout.readlines()
     print(lines,'\n')
     vm_stderr.close()
     vm_stdin.close()
     vm_stdout.close()
+
+def ping_vms(vm1,vm2_ip):
+    ping_cmd = "ping -c 3 " + vm2_ip
+    vm1_stdin, vm1_stdout, vm1_stderr = vm1.exec_command(ping_cmd)
+    lines = vm1_stdout.readlines()
+    print(lines,'\n')
+    vm1_stderr.close()
+    vm1_stdin.close()
+    vm1_stdout.close()
+
+def set_ip(vm,ip):
+    set_ip_cmd = "sudo ifconfig enp0s8 " + ip + " up"
+    vm.exec_command(set_ip_cmd)
+    vm.exec_command('sudo service network-manager restart')
+    time.sleep(1)
 
 vm_u = paramiko.SSHClient()
 vm_u2 = paramiko.SSHClient()
@@ -18,54 +33,44 @@ vm_u2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 vm_u.connect('10.13.6.103',port=22, username='oliveruhlar', password='', key_filename='w_rsa')
 vm_u2.connect('10.13.6.106',port=22, username='oliveruhlar', password='', key_filename='vm_u2_rsa')
+vm_u.exec_command('sudo ifconfig enp0s8 down')
+vm_u2.exec_command('sudo ifconfig enp0s8 down')
+time.sleep(1)
 
-print('\n',"Interface configuration berfore setting interfaces and ip for Internal Network",'\n')
+print('\n','\n',"Interface configuration berfore setting interfaces and ip for Internal Network",'\n')
 print("VM n.1")
-vm_u_stdin, vm_u_stdout, vm_u_stderr = vm_u.exec_command("ip -o address")
-lines = vm_u_stdout.readlines()
-print(lines,'\n')
+print_ifconffig(vm_u)
 print("VM n.2")
-vm_u2_stdin, vm_u2_stdout, vm_u2_stderr = vm_u2.exec_command("ip -o address")
-lines = vm_u2_stdout.readlines()
-print(lines)
+print_ifconffig(vm_u2)
+
+print('\n', "Ping Vms before setting IPs for Internal network",'\n')
+
+print('ping VM n.1 -> VM n.2')
+ping_vms(vm_u,"10.2.0.21")
+
+print('ping VM n.2 -> VM n.1')
+ping_vms(vm_u2,"10.2.0.20")
 
 
 print('\n',"Interface configuration after setting interfaces and ip for Internal Network",'\n')
 
 print("VM n.1")
-vm_u.exec_command('sudo ifconfig enp0s8 10.2.0.20 up')
-vm_u.exec_command('sudo service network-manager restart')
-time.sleep(1)
-vm_u_stdin, vm_u_stdout, vm_u_stderr = vm_u.exec_command("ip -o address")
-lines = vm_u_stdout.readlines()
-print(lines,'\n')
+set_ip(vm_u,"10.2.0.22")
+print_ifconffig(vm_u)
 
 print("VM n.2")
 vm_u2.exec_command('sudo ifconfig enp0s8 10.2.0.21 up')
 vm_u2.exec_command('sudo service network-manager restart')
 time.sleep(1)
-vm_u2_stdin, vm_u2_stdout, vm_u2_stderr = vm_u2.exec_command("ip -o address")
-lines = vm_u2_stdout.readlines()
-print(lines)
+print_ifconffig(vm_u2)
 
 print('\n',"Ping VMs between each other",'\n')
 
 print('ping VM n.1 -> VM n.2')
-vm_u_stdin, vm_u_stdout, vm_u_stderr = vm_u.exec_command('ping -c 3 10.2.0.21')
-lines = vm_u_stdout.readlines()
-print(lines,'\n')
+ping_vms(vm_u,"10.2.0.21")
 
 print('ping VM n.2 -> VM n.1')
-vm_u2_stdin, vm_u2_stdout, vm_u2_stderr = vm_u2.exec_command('ping -c 3 10.2.0.20')
-lines = vm_u2_stdout.readlines()
-print(lines)
+ping_vms(vm_u2,"10.2.0.20")
 
-vm_u_stderr.close()
-vm_u_stdin.close()
-vm_u_stdout.close()
 vm_u.close()
-
-vm_u2_stderr.close()
-vm_u2_stdin.close()
-vm_u2_stdout.close()
 vm_u2.close()
