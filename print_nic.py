1f9f503c513f43c1c96aba303195c3c88e486126
oliveruhlar <oliver.uhlar@pantheon.tech>
2020-12-09 07:41:37 +0100

    table ifconfig change

--- print_nic.py
+++ print_nic.py
@@ -6,7 +6,7 @@ def print_ifconffig(vm):
     vm_stdin, vm_stdout, vm_stderr = vm.exec_command(str("""ip link | awk -F: '$0 !~ "lo|vir|^[^0-9]"{print $2a;getline}'"""))
     ip_addrs = [i.strip() for i in vm_stdout1.readlines()]
     nic_names = [i.strip() for i in vm_stdout.readlines()]
-    rows = [list(i) for i in zip(ip_addrs, nic_names)]
+    rows = [list(i) for i in zip(nic_names, ip_addrs)]
     nic_tb = PrettyTable(["NIC name","IPv4"])
     for row in rows:
         nic_tb.add_row(row)
